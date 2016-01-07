6b0dec008dd522568ea4390bbfd5a5e3487e9bf9
dmaza <dayllan.maza@gmail.com>
2016-01-07 03:50:36 +0000

    adding doctrine config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,29 @@
 <?php
 /**
- * Global Configuration Override
+ * Local Configuration Override
  *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
  *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
  */
 
 return array(
-    // ...
+	'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => 'root',
+                    'dbname'   => 'reservations',
+                )
+            )
+        )
+    ),
 );
